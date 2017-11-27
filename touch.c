111da3cbf2c3ec72bf0218b340280c3ee7bfcaf3
brofreecss2 <jonathan16@mhs.if.its.ac.id>
2017-11-27 10:25:31 +0700

    fix and change implementation

--- touch.c
+++ touch.c
@@ -1,19 +1,29 @@
-#include <stdio.h>
+#include "types.h"
+#include "user.h"
+#include "fcntl.h"
 
 /*
 Checklist:
 - cek kalau ada parameter
+- cek kalau ada input yang salah
+- membuat beberapa file sekaligus
 */
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
 	if(argc==1){
-		puts("touch: missing file operand");
-		return 0;
+		//command invalid, hanya memanggil nama command saja
+		printf(1,"invalid command\n");
+	}
+	else{
+		if(argc==2){
+			int fd = open(argv[1],O_CREATE | O_RDWR);
+			if(fd >= 0){
+				printf(1,"Create file succeed!\n");
+			}
+			close(fd);
+		}
 	}
 
-	FILE* out = fopen(argv[1],"w");
-	fclose(out);
-
-	return 0;
+	exit();
 }
