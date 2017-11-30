fa7c41be0a1a71ecbd9fe7dce0e16ffebcd66efd
brofreecss2 <jonathan16@mhs.if.its.ac.id>
2017-11-30 21:55:01 +0700

    fix implementation, added EOF flag

--- tee.c
+++ tee.c
@@ -1,21 +1,46 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 /*
 Checklist
-- Masih belum bisa handle SIGSTOP atau SIGKILL
 - Tambah fungsi untuk cek parameter
 */
 
+char buffer[1025];
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
-	char buffer[1025];
-	char print[1025];
+	int fd,n;
+	if(argc==2){
+		if((fd = open(argv[1], O_CREATE | O_RDWR)) < 0){
+			printf(1,"tee: cannot open %s\n",argv[1]);
+			exit();
+		}
+	}
 
-	print = gets(buffer,1024);
-	printf(1,"%s\n",print);
+	while(1){
+		n = read(0,buffer,sizeof buffer);
+		if(n==0)break;
+		if(strcmp(buffer,"")==0)break;
 
+		if(n < 0){
+			printf(1,"tee: read error\n");
+			exit();
+		}
+
+		if(write(1,buffer,n) != n){
+			printf(1,"write error\n");
+		}
+		if(argc==2){
+			if(write(fd,buffer,n) != n){
+				printf(1,"tee: write error\n");
+			}
+		}
+	}
+	
+	if(argc==2)close(fd);
+	printf(1,"\n");
+	exit();
 }
