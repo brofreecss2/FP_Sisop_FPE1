ca3aa1feed5699024cc9acfd6a8abbb0ae158fda
Unknown <yolandahertita903@gmail.com>
2017-12-05 15:26:01 +0700

    fix

--- env.c
+++ env.c
@@ -1,41 +1,15 @@
-// Because xv6 doesn't set the environ, we simulate it here
-#include <stdio.h>
-#include <string.h>
-#define ENVLEN 100
-#define ENV_FILE "/etc/env"
-//#define ENV_FILE "/home/wkt/xv6-freebsd/fs/etc/env"
-#define ENV_LINE_LEN 200
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "fcntl.h"
 
-static char * xv6env[ENVLEN];
-extern char **environ;
-
-void loadxv6env()
+int main(int argc, char *argv[])
 {
-  FILE *in;
-  char env_line[ENV_LINE_LEN];
-  int posn=0;
+  char *env[] = environ;
+  int i = 0;
 
-    // Try to open the env file
-  if ((in=fopen(ENV_FILE, "r"))==NULL) return;
-
-  while (fgets(env_line, ENV_LINE_LEN-1, in)!=NULL) {
-    // Ignore comments
-    if (env_line[0] == '#') continue;
-
-    // Stop if we run out of room
-    if (posn==ENVLEN) break;
-
-    // Remove the newline
-    env_line [ strlen(env_line) - 1] = '\0';
-
-    // Copy the variable into the array
-    xv6env[posn++]=strdup(env_line);
+  for(; env[i] != 0; i++){
+    printf(1, "%s\n", env[i]);
   }
-  fclose(in);
-  environ= xv6env;
-  return;
-}
-
-int main(){
-	loadxv6env();
+  exit();
 }
