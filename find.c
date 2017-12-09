b39c6df2a3112cfec394bff313b783cf74160c86
brofreecss2 <jonathan16@mhs.if.its.ac.id>
2017-12-09 10:58:11 +0700

    fix bug

--- find.c
+++ find.c
@@ -83,11 +83,8 @@ void ls(char *path, char *needle){
 
 int main(int argc, char *argv[])
 {
-	if(argc == 1){
+	if(argc == 2){
 		ls(".",argv[argc-1]);
-	}
-	else if(argc == 3){
-		ls(argv[1],argv[2]);
 	}
 	else{
 		for(int i=1;i<argc-1;++i){
